1d76c11b6e75f3205fff08001a12298b75492990
tobias <tobias@tobiass-MacBook-Pro.local>
2020-07-08 17:55:04 +0200

    uh

--- init-go-cli.sh
+++ init-go-cli.sh
@@ -1,3 +1,4 @@
 go mod init github.com/tobias-ericsson/unified-help
 go get -u github.com/spf13/cobra/cobra
-cobra init uh
+cobra init uh --pkg-name github.com/tobias-ericsson/unified-help/uh
+go install github.com/tobias-ericsson/unified-help/uh
